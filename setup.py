9de910258523afa60660225bd47e5c62e99c77fc
embray <embray@stsci.edu>
2011-05-19 21:22:17 +0000

    Adds a workaround for a zany problem with setup_requires, where multiple packages have the same requirement in setup_requires.  The comment in setup.py provides links to more detailed descriptions of the problem.
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/branches/setup_refactoring/distutils@12888 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -7,8 +7,27 @@ except ImportError:
     use_setuptools()
     from setuptools import setup
 
-setup(
-    setup_requires=['d2to1'],
-    d2to1=True,
-    use_2to3=True
-)
+from pkg_resources import working_set
+
+
+# This is a workaround for http://bugs.python.org/setuptools/issue20; most
+# packages that have this package as a setup-requirement also have d2to1 as
+# a setup-requirement, which can lead to bugginess.
+# See also http://mail.python.org/pipermail/distutils-sig/2011-May/017812.html
+# for a description of the problem (in my example, package_A is d2to1 and
+# package_B is stsci.distutils).
+
+save_entries = working_set.entries[:]
+save_entry_keys = working_set.entry_keys.copy()
+save_by_key = working_set.by_key.copy()
+
+try:
+    setup(
+        setup_requires=['d2to1'],
+        d2to1=True,
+        use_2to3=True
+    )
+finally:
+    working_set.entries = save_entries
+    working_set.entry_keys = save_entry_keys
+    working_set.by_key = save_by_key
