378b5f9b36ab6291aae9112f2e2ae99dd8959a36
embray <embray@stsci.edu>
2011-09-02 22:08:21 +0000

    Fixing the dependency on distribute specifically
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/branches/distutils-standalone@13759 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -19,8 +19,10 @@ from pkg_resources import working_set, get_distribution, Requirement
 # This issue was fixed in distribute 0.6.17, but leaving in support for older
 # versions for now.
 requirement = Requirement.parse('distribute<0.6.19')
-dist = get_distribution('setuptools')
-has_issue205 = dist.project_name == 'distribute' and dist in requirement
+try:
+    has_issue205 = get_distribution('distribute') in requirement
+except:
+    has_issue205 = False
 
 if has_issue205:
     save_entries = working_set.entries[:]
