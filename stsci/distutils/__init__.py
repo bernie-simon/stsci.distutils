3923413c56f81d19adf429f39a5091341e34d91c
sienkiew <sienkiew@stsci.edu>
2013-02-06 20:29:42 +0000

    get version from new install
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci.distutils/trunk@22265 fe389314-cf27-0410-b35b-8c050e845b92

--- stsci/distutils/__init__.py
+++ stsci/distutils/__init__.py
@@ -1,8 +1,2 @@
-try:
-    from .version import (__version__, __svn_revision__, __svn_full_info__,
-                          __setup_datetime__)
-except ImportError:
-    __version__ = ''
-    __svn_revision__ = ''
-    __svn_full_info__ = ''
-    __setup_datetime__ = None
+from .version import *
+
